35cb53271b29a8677d393f62ae5a1795a29d2351
GanLiuuuu <119108256+GanLiuuuu@users.noreply.github.com>
2024-05-14 14:47:45 +0800

    update

--- TOP.v
+++ TOP.v
@@ -26,11 +26,11 @@ wire[`REGWIDTH-1:0] PCout;
 wire[`REGWIDTH-1:0] cur_PC;
 wire[`REGWIDTH-1:0] PC;
 
-
+getWriteData GetWriteData();
 PC pc(.Addr_result(PCout), .clock(clk), .reset(rst_filtered), .Branch(Branch), .Zero(zero), .branch_base_addr(cur_PC),  .PC(PC));
-IFetch iFetch(.clk(clk), .rst(rst_filtered), .PC(PC), .instruction(inst));
+instruction_fetch iFetch(.clk(clk), .rst(rst_filtered), .PC(PC), .instruction(inst));
 Controller controller(.inst(inst),.Branch(Branch), .ALUOp(ALUOp), .ALUSrc(ALUSrc), .ALUSrc1(ALUSrc1), .PCSrc(PCSrc), .MemRead(MemRead), .MemWrite(MemWrite), .MemtoReg(MemtoReg), .RegWrite(RegWrite));
 Decoder decoder(.clk(clk), .rst(rst), .instruction(instruction), .WriteData(WriteData), .Write(Write), .imm(imm), .ReadData1(ReadData1), .ReadData2(ReadData2));
 ALU alu(.PCin(cur_PC), .ALUSrc(ALUSrc), .ALUSrc1(ALUSrc1), .PCSrc(PCSrc), .ALUOp(ALUOp), .funct3(inst[14:12]), .funct7(inst[31:25]), .ReadData1(ReadData1), .ReadData2(ReadData2), .imm32(imm), .zero(zero), .ALUResult(ALUResult), .PCout(PCout));
-
+Data_Mamory dma(.clk(clk), .MemRead(MemRead), .MemWrite(MemWrite), .addr(ALUResult), .din(ReadData2), .dout())
 endmodule
