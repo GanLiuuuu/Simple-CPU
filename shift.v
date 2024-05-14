208a4ca67b279721fcd03ec56850e2b0fd4cc34f
Gan Lau <119108256+GanLiuuuu@users.noreply.github.com>
2024-05-14 23:54:47 +0800

    Update shift.v

--- shift.v
+++ shift.v
@@ -5,13 +5,10 @@ module shift(
     input shift_type,
     input[`REGWIDTH-1:0] rs1,
     input[`REGWIDTH-1:0] rs2,
-    output[`REGWIDTH-1:0] rd
-)
-assign rd = (shift_dir==`SHIRFTRIGHT) ? 
-(
-    (shift_type==`SHIRFTLOGIC) ? rs1 >> rs2 : rs1 >>> rs2
-):
-(
-    rs1 << rs2
+    output [`REGWIDTH-1:0] rd
 );
+
+assign rd = (shift_dir == `SHIRFTRIGHT && shift_type == `SHIRFTLOGIC) ? (rs1 >> rs2) :
+            (shift_dir == `SHIRFTRIGHT) ? (rs1 >>> rs2) : (rs1 << rs2);
+
 endmodule
