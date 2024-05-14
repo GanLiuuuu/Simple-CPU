7f8fe5bdc5110307c78dd0e2a2c134e3e79d9778
GanLiuuuu <119108256+GanLiuuuu@users.noreply.github.com>
2024-05-14 18:19:18 +0800

    tb

--- ALUtb.v
+++ ALUtb.v
@@ -8,40 +8,23 @@ wire Branch;
 wire[31:0] ALUOp;
 wire[31:0] ALUSrc;
 wire[31:0] ALUSrc1;
-output PCSrc;//choose the first operand, 0 for PC, 1 for rs1
-output MemRead;
-output MemWrite;
-output MemtoReg;
-output RegWrite;
-Controller ctl(inst,Branch,ALUOp,ALUSrc,ALUSrc1,PCSrc,MemRead,MemWrite,MemtoReg,RegWrite);
+wire PCSrc;//choose the first operand, 0 for PC, 1 for rs1
+wire MemRead;
+wire MemWrite;
+wire MemtoReg;
+wire RegWrite;
+wire[31:0] ReadData1;
+wire[31:0] ReadData2;
+wire[31:0] imm32;
+wire zero;
+wire[31:0] ALUResult;
+wire[31:0] WriteData;
+Controller ctl(instruction,Branch,ALUOp,ALUSrc,ALUSrc1,PCSrc,MemRead,MemWrite,MemtoReg,RegWrite);
+ALU alu(PC,ALUSrc,ALUSrc1,PCSrc,ALUOp,instruction[14:12],instruction[31:25], ReadData1, ReadData2, imm32, zero, ALUResult,PCout);
 //不涉及对内存的处理
 //PC值为默认的0
+Decoder decoder(clk,rst,instruction,WriteData,RegWrite,imm32,ReadData1,ReadData2);
 
- ALU(
-    input[`REGWIDTH-1:0] PCin,
-    input ALUSrc,
-    input ALUSrc1,
-    input PCSrc,
-    input[`ALUOPWIDTH-1:0] ALUOp,
-    input[2:0] funct3,
-    input[6:0] funct7,
-    input[`REGWIDTH-1:0] ReadData1,
-    input[`REGWIDTH-1:0] ReadData2,
-    input[`REGWIDTH-1:0] imm32,
-    output zero,
-    output[`REGWIDTH-1:0] ALUResult,
-    output[`REGWIDTH-1:0] PCout
-);
- Decoder(
-input clk,
-input rst,
-input[`REGWIDTH-1:0] instruction,
-input[`REGWIDTH-1:0] WriteData,
-input Write,
-output reg[`REGWIDTH-1:0] imm,
-output reg [`REGWIDTH-1:0] ReadData1,
-output reg [`REGWIDTH-1:0] ReadData2
-);
 initial begin
     clk = 1'b0;
     rst = 1'b1;
--- TOP.v
+++ TOP.v
@@ -18,7 +18,6 @@ wire MemWrite;
 wire MemtoReg;
 wire RegWrite;
 wire[`REGWIDTH-1:0] WriteData;
-wire Write;
 wire [`REGWIDTH-1:0] imm;
 wire[`REGWIDTH-1:0] ReadData1;
 wire[`REGWIDTH-1:0] ReadData2;
@@ -37,7 +36,7 @@ getWriteData GetWriteData(.mux_signal(MemtoReg), .ReadData(MemData), .ALUResult(
 PC pc(.Addr_result(PCout), .clock(clk), .reset(rst_filtered), .Branch(Branch), .Zero(zero), .branch_base_addr(cur_PC),  .PC(PC));
 instruction_fetch iFetch(.clk(clk), .rst(rst_filtered), .PC(PC), .instruction(inst));
 Controller controller(.inst(inst),.Branch(Branch), .ALUOp(ALUOp), .ALUSrc(ALUSrc), .ALUSrc1(ALUSrc1), .PCSrc(PCSrc), .MemRead(MemRead), .MemWrite(MemWrite), .MemtoReg(MemtoReg), .RegWrite(RegWrite));
-Decoder decoder(.clk(clk), .rst(rst), .instruction(instruction), .WriteData(WriteData), .Write(Write), .imm(imm), .ReadData1(ReadData1), .ReadData2(ReadData2));
+Decoder decoder(.clk(clk), .rst(rst), .instruction(instruction), .WriteData(WriteData), .Write(RegWrite), .imm(imm), .ReadData1(ReadData1), .ReadData2(ReadData2));
 ALU alu(.PCin(cur_PC), .ALUSrc(ALUSrc), .ALUSrc1(ALUSrc1), .PCSrc(PCSrc), .ALUOp(ALUOp), .funct3(inst[14:12]), .funct7(inst[31:25]), .ReadData1(ReadData1), .ReadData2(ReadData2), .imm32(imm), .zero(zero), .ALUResult(ALUResult), .PCout(PCout));
 Data_Mamory dma(.clk(clk), .MemRead(MemRead), .MemWrite(MemWrite), .addr(ALUResult), .din(ReadData2), .dout(MemData));
 assign out=ALUResult;
