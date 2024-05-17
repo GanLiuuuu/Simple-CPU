fe1e43a64f40e6978e165bd4e68194a9e3183cdd
viayu <1761278446@qq.com>
2024-05-17 17:25:27 +0800

    Update memOrIO.v

--- memOrIO.v
+++ memOrIO.v
@@ -1,39 +1,57 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
+module MemOrIO (
+    input mRead,  // read memory, from control32 
+    input mWrite,  // write memory, from control32 
+    input ioRead,  // read IO, from control32 
+    input ioWrite,  // write IO, from control32 
+    input [31:0] m_rdata,  // data read from memory 
+    input [31:0] r_rdata,  // data read from idecode32(register file) 
+    input [31:0] addr_in,  // from alu_result in executs32 
 
-module MemOrIO(input mRead,                  // Read memory, from Controller
-               input mWrite,                 // Write memory, from Controller
-               input IORead,                 // Read IO, from Controller
-               input IOWrite,                // Read IO, from Controller
-               
-               input[31:0] addr_in,          // From Alu_result in ALU
-               output[31:0] addr_out,        // Address to Data-Memory
-               
-               input[31:0] m_rdata,          // Data read from Data-Memory
-               input[15:0] io_rdata,         // Data read from IO, 16 bits
-               output[31:0] r_wdata,         // Data to Decoder(register file)
-               
-               input[31:0] r_rdata,          // Data read from Decoder(register file)
-               output reg [31:0] write_data, // Data to memory or I/O
-               output reg [31:0] write_data_seg, // Data to I/O
-               output LEDCtrl,               // LED Chip Select
-               output SEGCtrl,               // SEG Chip Select
-               output SwitchCtrl);           // Switch Chip Select
+    input [15:0] io_rdata_switch,  // data read from switch,16 bits
+    input [15:0] io_rdata_board,  // data read from board,16 bits
+    input io_rdata_btn,
+
+    output [31:0] addr_out,  // address to memory 
+    output [31:0] r_wdata,  // data to idecode32(register file) 
+
+    output reg [31:0] write_data,  // data to memory or I/O（m_wdata, io_wdata） 
+
+    output LEDCtrlhigh,  // LED Chip Select
+    output LEDCtrllow,  // LED Chip Select
     
-    assign addr_out   = addr_in;
-    assign r_wdata    = (IORead == 1)?{16'h0000,io_rdata}:m_rdata; // may from memory or I/O, if from I/O, it is rdata's lower 16bit
-    assign LEDCtrl    = (IOWrite == 1'b1 && addr_in[7:4] == 4'b0110)?1'b1:1'b0; // ledCtrl, 1 is effective;
-    assign SEGCtrl    = (IOWrite == 1'b1 && addr_in[7:4] == 4'b0101)?1'b1:1'b0; //segCtrl, 1 is effective
-    assign SwitchCtrl = (IORead == 1'b1 && addr_in[7:4] == 4'b0111)?1'b1:1'b0; //switchCtrl, 1 is effective
-    
-    always @*begin
-        if ((mWrite == 1'b1)||(IOWrite == 1'b1)) begin
-            write_data = ((mWrite == 1'b1)?r_rdata:{16'h0000,r_rdata[15:0]});
-            write_data_seg = {16'h0000,r_rdata[15:0]};
-            end
-        else begin
-            write_data = 32'hZZZZ_ZZZZ;
-            write_data_seg = 32'hZZZZ_ZZZZ;
+    output SwitchCtrl,  //拨码开关
+    output SegCtrl  //七段数码显示管
+
+);
+
+    assign addr_out = addr_in;
+    wire ck_in;
+
+    wire [3:0] low_addr = addr_in[3:0];
+    reg [15:0] io_rdata;
+
+    assign r_wdata = mRead ? m_rdata : {16'h0000, io_rdata};
+    assign LEDCtrlhigh = (ioWrite && low_addr == 4'h0) ? 1'b1 :1'b0;
+     assign LEDCtrllow = (ioWrite && low_addr == 4'h4) ? 1'b1 :1'b0;
+    assign SegCtrl = (ioWrite && low_addr == 4'h8) ?  1'b1 : 1'b0;
+
+    assign SwitchCtrl = (ioRead && low_addr == 4'h0) ?  1'b1 :1'b0;
+    assign ckin = (ioRead && low_addr == 4'h4) ?  1'b1 :1'b0;
+
+
+    always @(*) begin
+        if (SwitchCtrl) begin
+            io_rdata = io_rdata_switch;
+        end else if (ckin) begin
+            io_rdata = {15'b0, io_rdata_btn};
+        end
+    end
+
+    always @(*) begin
+        if (mWrite || ioWrite) begin
+            write_data = r_rdata;
+        end else begin
+            write_data = 32'hZZZZZZZZ;
         end
     end
 endmodule
