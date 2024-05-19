994c8859daa7aa183aa0bb81008aa04ec364026a
Gan Lau <119108256+GanLiuuuu@users.noreply.github.com>
2024-05-19 14:46:47 +0800

    Create TOPtb.v

---
+++ TestBench/TOPtb.v
@@ -0,0 +1,12 @@
+module Toptb();
+reg rst;
+reg clk;
+wire[31:0] out;
+Top top(clk,rst,out);
+initial begin
+    clk = 1'b0;
+    rst = 1'b0;
+    #30 rst = 1'b1;
+end
+always begin #10 clk = ~clk; end
+endmodule
