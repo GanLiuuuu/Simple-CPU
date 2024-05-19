5e9f2dd286f53b52099e8bbf48b6a1da2580a192
Gan Lau <119108256+GanLiuuuu@users.noreply.github.com>
2024-05-19 22:19:13 +0800

    Update Data_Mamory.v

--- Data_Mamory.v
+++ Data_Mamory.v
@@ -15,10 +15,10 @@ module Data_Mamory(
     assign read_mem = (addr[31]==1) ? 1'b1 : 1'b0;
     wire write_mem;
     assign write_mem = (addr[31]!=1'b1 && MemWrite==1'b1) ? 1'b1:1'b0;
-    // �ڲ�����
+    // 锟节诧拷锟斤拷锟斤拷
     wire [31:0] internal_data;
 
-    // ����RAMģ��
+    // 锟斤拷锟斤拷RAM模锟斤拷
     RAM ram (
         .addra(addr[13:0]),
         .clka(clk),
@@ -27,16 +27,22 @@ module Data_Mamory(
         .wea(write_mem)
     );
     //read data
-    always @(negedge clk)begin
-        if(read_mem)begin
-            dout <= {15'b0,io_rdata_switch};
+    always @(negedge clk,posedge rst)begin
+        if(rst)begin
+            dout <= 32'b0;
         end
         else begin
-            dout <= internal_data;
+                if(read_mem)begin
+                    dout <= {15'b0,io_rdata_switch};
+                end
+                else begin
+                    dout <= internal_data;
+                end
         end
+        
     end
     always @(posedge clk,posedge rst) begin
-        if(!write_mem&& MemWrite)begin
+        if(!write_mem && MemWrite)begin
             LED<=din[15:0];
         end
         if(rst)begin
