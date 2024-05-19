f5f2689a53fdb1cb89f6e93d528a8baf8906bb6d
Gan Lau <119108256+GanLiuuuu@users.noreply.github.com>
2024-05-19 22:08:53 +0800

    Update PC.v

--- PC.v
+++ PC.v
@@ -2,13 +2,13 @@
 
 module PC(
 
-    input[31:0]  Addr_result,            // 来自ALU,为ALU计算出的跳转地址(beq)
-    input        clock,           //时钟
-    input        reset,           //复位信号高电平有效
-    input        Branch,               // 来自controller（判断是否是beq）
-    input        Zero,                  //来自ALU（判断是否相等）
+    input[31:0]  Addr_result,            // 鏉ヨ嚜ALU,涓篈LU璁＄畻鍑虹殑璺宠浆鍦板潃(beq)
+    input        clock,           //鏃堕挓
+    input        reset,           //澶嶄綅淇″彿楂樼數骞虫湁鏁�
+    input        Branch,               // 鏉ヨ嚜controller锛堝垽鏂槸鍚︽槸beq锛�
+    input        Zero,                  //鏉ヨ嚜ALU锛堝垽鏂槸鍚︾浉绛夛級
      
-    output[31:0] branch_base_addr,   // 有条件跳转指令，该值为PC，送往ALU
+    output[31:0] branch_base_addr,   // 鏈夋潯浠惰烦杞寚浠わ紝璇ュ�间负PC锛岄�佸線ALU
     output reg [31:0] PC
     );
     wire[31:0] PC_plus_4;
@@ -17,13 +17,13 @@ module PC(
     assign PC_plus_4 = PC + 4;
     assign branch_base_addr = PC;
     always @* begin
-        if(((Branch == 1) && (Zero == 1 )))  //跳转
+        if(((Branch == 1) && (Zero == 1 )))  //璺宠浆
             Next_PC = Addr_result;
         else 
             Next_PC = PC_plus_4; // PC+4
     end
 
-    always @(negedge clock or posedge reset) begin
+    always @(posedge clock or posedge reset) begin
         if(reset == 1)
             PC <= 32'b0;
         else
