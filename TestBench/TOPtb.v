51ac52743875c1f6a7df1cb4e0c6e4606d1128be
Gan Lau <119108256+GanLiuuuu@users.noreply.github.com>
2024-05-19 17:15:38 +0800

    Update TOPtb.v

--- TestBench/TOPtb.v
+++ TestBench/TOPtb.v
@@ -6,8 +6,8 @@ wire[31:0] out;
 TOP top(clk,rst,out);
 initial begin
     clk = 1'b0;
-    rst = 1'b0;
-    #30 rst = 1'b1;
+    rst = 1'b1;
+    #30 rst = 1'b0;
 end
 always begin #10 clk = ~clk; end
 endmodule
