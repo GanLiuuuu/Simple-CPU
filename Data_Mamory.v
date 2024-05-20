ca3e3244250318fd375ff765bbf721cf8022d154
Gan Lau <119108256+GanLiuuuu@users.noreply.github.com>
2024-05-20 20:35:45 +0800

    Update Data_Mamory.v

--- Data_Mamory.v
+++ Data_Mamory.v
@@ -12,9 +12,11 @@ module Data_Mamory(
     input [15:0] io_rdata_switch,
     output reg [31:0] dout,
     output reg [15:0] LED
+
     );
+      wire[15:0] tmp3;
     wire read_mem;
-    assign read_mem = (addr[31]==1) ? 1'b1 : 1'b0;
+    assign read_mem = (addr[31]==1) ? 1'b0 : 1'b1;
     wire write_mem;
     assign write_mem = (addr[31]!=1'b1 && MemWrite==1'b1) ? 1'b1:1'b0;
     // 閿熻妭璇ф嫹閿熸枻鎷烽敓鏂ゆ嫹
@@ -30,7 +32,7 @@ module Data_Mamory(
     assign read_data= (length==2'b10)? tmp : (length==2'b01&&sign==1'b1)? {{16{tmp[15]}},tmp[15:0]}:(length==2'b01&&sign==1'b0)?{16'd0,tmp[15:0]}:(length==2'b00&&sign==1'b1)?{{24{tmp[7]}},tmp[7:0]}:{24'd0,tmp[7:0]};
     assign write_data=(length==2'b10)? din : (length==2'b01)? {16'd0,din[15:0]}:{24'd0,din[7:0]};
 
-    wire[15:0] tmp3;//data written back to led
+   //data written back to led
     assign tmp3 = write_data[15:0]<<<a;
     RAM ram (
         .addra(addr[13:0]),
