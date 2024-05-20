f811770695e65d7f9c75eff6c1e96777573d7047
Gan Lau <119108256+GanLiuuuu@users.noreply.github.com>
2024-05-20 23:36:48 +0800

    Create scene2.asm

---
+++ TestCases/scene2.asm
@@ -0,0 +1,39 @@
+.text
+li t6,0xfffff000#base address of swithches
+li t5,0xfffff010#base address of LED
+main:
+lb t0,12(t6)#get the number of testcase
+#todo: test case 000-100
+
+li t1,5#0101
+beq t0,t1,test101
+#todo:testcase110-111
+
+j main
+
+test101:
+#li a7,5
+#ecall
+lh a0,0(t6)
+srli a0,a0,4
+add t0,x0,a0
+li t1,255
+and a0,t0,t1
+slli a0,a0,24
+li t1, 65280
+and t2, t0,t1
+slli t2,t2,8
+or a0,a0,t2
+li t1, 16711680
+and t2,t0,t1
+srai t2,t2,8
+or a0,a0,t2
+srai t0,t0,24
+li t1,255
+and t0,t0,t1
+or a0,t0,a0
+srli a0,a0,16
+sh a0,0(t6)
+j main
+#li a7,1
+#ecall
