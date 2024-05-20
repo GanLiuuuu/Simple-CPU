74bec6ff7ba947b497b8e07061f8575489454565
Gan Lau <119108256+GanLiuuuu@users.noreply.github.com>
2024-05-20 14:16:01 +0800

    Update TOPtb.v

--- TestBench/TOPtb.v
+++ TestBench/TOPtb.v
@@ -5,12 +5,13 @@ reg clk;
 wire[31:0] out;
 reg[15:0] switches;
 wire[15:0] LED;
+
 TOP top(clk,rst,switches,out,LED);
 initial begin
     switches = 16'b0;
     clk = 1'b0;
     rst = 1'b1;
-    #30 rst = 1'b0;
+    #5 rst = 1'b0;
 end
 always begin #10 clk = ~clk; end
 endmodule
