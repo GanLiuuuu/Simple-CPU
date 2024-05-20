d87262e7d74a26d59c0df9dabb47314114f45a14
Gan Lau <119108256+GanLiuuuu@users.noreply.github.com>
2024-05-20 16:21:55 +0800

    Update TOP.v

--- TOP.v
+++ TOP.v
@@ -4,7 +4,7 @@ module TOP(
     input clk,
     input rst,
     input[15:0] switches,
-    output[`REGWIDTH-1:0] out,
+    output[`REGWIDTH-1:0] out,//used for debuging
     output  [15:0] LED
     
     );
@@ -43,6 +43,10 @@ wire[`REGWIDTH-1:0] MemData;
 wire[`REGWIDTH-1:0] dout;
 
 wire[`REGWIDTH-1:0] ALUResult;
+
+wire[1:0] length;
+wire sign;
+
 wire en_reg;
 assign en = (state[2:1]==2'b00) ? 1'b1 : 1'b0;
 assign en_reg = (state==3'b110)? 1'b1:1'b0;
@@ -51,9 +55,9 @@ getWriteData GetWriteData(.mux_signal(MemtoReg), .ReadData(MemData), .ALUResult(
 PC pc(.en(en),.Addr_result(PCout), .clock(cpu_clk), .reset(rst_filtered), .Branch(Branch), .Zero(zero),  .PC(pPC));
 instruction_fetch iFetch(.clk(cpu_clk), .rst(rst_filtered), .PC(pPC), .instruction(inst));
 Controller controller(.inst(inst),.Branch(Branch), .ALUOp(ALUOp), .ALUSrc(ALUSrc), .ALUSrc1(ALUSrc1), .PCSrc(PCSrc), .MemRead(MemRead), .MemWrite(MemWrite), .MemtoReg(MemtoReg), .RegWrite(RegWrite));
-Decoder decoder(.en(en_reg),.clk(cpu_clk), .rst(rst), .instruction(inst), .WriteData(WriteData), .Write(RegWrite), .imm(imm), .ReadData1(ReadData1), .ReadData2(ReadData2));
+Decoder decoder(.length(length), .sign(sign),.en(en_reg),.clk(cpu_clk), .rst(rst), .instruction(inst), .WriteData(WriteData), .Write(RegWrite), .imm(imm), .ReadData1(ReadData1), .ReadData2(ReadData2));
 ALU alu(.PCin(pPC), .ALUSrc(ALUSrc), .ALUSrc1(ALUSrc1), .PCSrc(PCSrc), .ALUOp(ALUOp), .funct3(inst[14:12]), .funct7(inst[31:25]), .ReadData1(ReadData1), .ReadData2(ReadData2), .imm32(imm), .zero(zero), .ALUResult(ALUResult), .PCout(PCout));
-Data_Mamory dma(.clk(cpu_clk), .rst(rst), .io_rdata_switch(switches),.LED(LED), .MemRead(MemRead), .MemWrite(MemWrite), .addr(ALUResult), .din(ReadData2), .dout(MemData));
+Data_Mamory dma(.length(length), .sign(sign),.clk(cpu_clk), .rst(rst), .io_rdata_switch(switches),.LED(LED), .MemRead(MemRead), .MemWrite(MemWrite), .addr(ALUResult), .din(ReadData2), .dout(MemData));
 assign out=ALUResult;
 endmodule
 
