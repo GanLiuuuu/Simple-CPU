b48f3158012d2c717c4c1ac9f093427eae679242
Gan Lau <119108256+GanLiuuuu@users.noreply.github.com>
2024-05-20 14:02:09 +0800

    Update Decoder.v

--- Decoder.v
+++ Decoder.v
@@ -1,6 +1,7 @@
 `include "variables.vh"
 
 module Decoder(
+input en,
 input clk,
 input rst,
 input[`REGWIDTH-1:0] instruction,
@@ -19,8 +20,8 @@ assign opcode = instruction[`OPWIDTH-1:0];
 assign ReadReg1 = (opcode==`RTYPE||opcode==`IARITH||opcode==`ILOAD||opcode==`STYPE||opcode==`BTYPE||opcode==`JALR)?instruction[19:15]:5'b0;
 assign ReadReg2 = (opcode==`RTYPE||opcode==`STYPE||opcode==`BTYPE)?instruction[24:20]:5'b0;
 assign WriteReg = (opcode==`STYPE||opcode==`BTYPE) ? 5'b0 : instruction[11:7];
-  assign imm = (opcode == `IARITH||opcode == `ILOAD) ?{{20{instruction[31]}},instruction[31:20]}:(opcode==`LUI||opcode==`AUIPC)?{instruction[31:12],12'b0}:(opcode==`STYPE)?{{20{instruction[31]}},instruction[31:25],instruction[11:7]}:(opcode==`BTYPE)?{{19{instruction[31]}},instruction[31],instruction[7],instruction[30:25],instruction[11:8],1'b0}:(opcode==`JAL)?{{11{instruction[31]}},instruction[31], instruction[19:12], instruction[20],instruction[30:21],1'b0}:(opcode==`JALR)?{20'b0,instruction[31:20]}:32'b0;
-Registers registers(clk,rst,Write,ReadReg1,ReadReg2,WriteReg,WriteData,ReadData1,ReadData2);
+assign imm = (opcode == `IARITH||opcode == `ILOAD) ?{{20{instruction[31]}},instruction[31:20]}:(opcode==`LUI||opcode==`AUIPC)?{instruction[31:12],12'b0}:(opcode==`STYPE)?{{20{instruction[31]}},instruction[31:25],instruction[11:7]}:(opcode==`BTYPE)?{{19{instruction[31]}},instruction[31],instruction[7],instruction[30:25],instruction[11:8],1'b0}:(opcode==`JAL)?{{11{instruction[31]}},instruction[31], instruction[19:12], instruction[20],instruction[30:21],1'b0}:(opcode==`JALR)?{20'b0,instruction[31:20]}:32'b0;
+Registers registers(en,clk,rst,Write,ReadReg1,ReadReg2,WriteReg,WriteData,ReadData1,ReadData2);
 
 
 endmodule
