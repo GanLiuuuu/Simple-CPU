f843b4e483381fef52b92a2b0598c03272cabda3
Gan Lau <119108256+GanLiuuuu@users.noreply.github.com>
2024-05-20 16:22:18 +0800

    Update Data_Mamory.v

--- Data_Mamory.v
+++ Data_Mamory.v
@@ -1,6 +1,8 @@
 `timescale 1ns / 1ps
 
 module Data_Mamory(
+    input [1:0] length,//0 for b, 1 for h, 2 for w
+    input sign,//0 for unsigned
     input clk,
     input rst,
     input MemRead,
@@ -15,35 +17,35 @@ module Data_Mamory(
     assign read_mem = (addr[31]==1) ? 1'b1 : 1'b0;
     wire write_mem;
     assign write_mem = (addr[31]!=1'b1 && MemWrite==1'b1) ? 1'b1:1'b0;
-    // 锟节诧拷锟斤拷锟斤拷
-    wire [31:0] internal_data;
+    // 閿熻妭璇ф嫹閿熸枻鎷烽敓鏂ゆ嫹
+    wire [31:0] tmp;
+    wire [31:0] tmp1;
+    wire [31:0] read_data;
+    wire [31:0] write_data;
+    assign tmp = (read_mem)?tmp1:{16'd0,io_rdata_switch};
+    assign read_data= (length==2'b10)? tmp : (length==2'b01&&sign==1'b1)? {{16{tmp[15]}},tmp[15:0]}:(length==2'b01&&sign==1'b0)?{16'd0,tmp[15:0]}:(length==2'b00&&sign==1'b1)?{{24{tmp[7]}},tmp[7:0]}:{24'd0,tmp[7:0]};
+    assign write_data=(length==2'b10)? din : (length==2'b01)? {16'd0,din[15:0]}:{24'd0,din[7:0]};
 
-    // 锟斤拷锟斤拷RAM模锟斤拷
     RAM ram (
         .addra(addr[13:0]),
         .clka(clk),
-        .dina(din),
-        .douta(internal_data),
+        .dina(write_data),
+        .douta(tmp1),
         .wea(write_mem)
     );
     //read data
     always @(negedge clk,posedge rst)begin
         if(rst)begin
-            dout <= 32'b0;
+            dout <= 32'd0;
         end
         else begin
-                if(read_mem)begin
-                    dout <= {15'b0,io_rdata_switch};
-                end
-                else begin
-                    dout <= internal_data;
-                end
+             dout<=read_data;
         end
         
     end
     always @(posedge clk,posedge rst) begin
         if(!write_mem && MemWrite)begin
-            LED<=din[15:0];
+            LED<=write_data[15:0];
         end
         if(rst)begin
             LED <= 16'b0;
