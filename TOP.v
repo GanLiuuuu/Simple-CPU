b5677ec253e232b651d477c7d635c1ae0ac5bde3
GanLiuuuu <119108256+GanLiuuuu@users.noreply.github.com>
2024-05-25 19:26:00 +0800

    7segment

--- TOP.v
+++ TOP.v
@@ -7,15 +7,20 @@ module TOP(
    // output[`REGWIDTH-1:0] out,//used for debuging
     output  [15:0] LED,
     output[7:0] digital_light,
+    output[7:0] digital_light1,
+
     output[7:0] seg_en,
     input button
     );
     wire rst;
     assign rst=~rst_a;
-    wire[31:0] input_32bit;
-    assign input_32bit = {{16{LED[15]}},LED};
+    wire[15:0] input_32bit;
+    assign input_32bit = LED;
     digital_presenter dp(clk,rst,input_32bit,digital_light,seg_en);
-    
+    wire[15:0] input_32bit1;
+    assign input_32bit1 = {16{LED[15]}};
+    wire[7:0] seg_en1;
+    digital_presenter dp1(clk,rst,input_32bit1,digital_light1,seg_en1);
      wire[`REGWIDTH-1:0] pPC;
      wire[`REGWIDTH-1:0] inst;
      wire [`REGWIDTH-1:0] imm;
--- digital_presenter.v
+++ digital_presenter.v
@@ -3,13 +3,13 @@
 module digital_presenter(
     input clk,
     input rst,
-    input [31:0] input_32bit,
+    input [16:0] input_16bit,
     output reg [7:0] digital_light,
-    output reg [7:0] seg_en
+    output reg [3:0] seg_en
 );
 
     reg [3:0] digit;
-    reg [2:0] digit_sel;
+    reg [1:0] digit_sel;
     wire [7:0] segment_output;
 
     seven_segment_display ssd (
@@ -19,24 +19,21 @@ module digital_presenter(
 
     always @(posedge clk or posedge rst) begin
         if (rst) begin
-            digit_sel <= 3'b000;
-            seg_en <= 8'b00000001;//默认输出是8，给第一个灯亮灯
+            digit_sel <= 2'b00;
+            seg_en <= 4'b1110;//默认输出是8，给第一个灯亮灯
         end else begin
             digit_sel <= digit_sel + 1;
-            seg_en <= {seg_en[6:0], seg_en[7]};  // 循环左移，激活下一个数码管
+            seg_en <= {seg_en[3:0], seg_en[4]};  // 循环左移，激活下一个数码管
         end
     end
 
     always @* begin
         case (digit_sel)
-            3'b000: digit = input_32bit[3:0];
-            3'b001: digit = input_32bit[7:4];
-            3'b010: digit = input_32bit[11:8];
-            3'b011: digit = input_32bit[15:12];
-            3'b100: digit = input_32bit[19:16];
-            3'b101: digit = input_32bit[23:20];
-            3'b110: digit = input_32bit[27:24];
-            3'b111: digit = input_32bit[31:28];
+            2'b00: digit = input_32bit[3:0];
+            2'b01: digit = input_32bit[7:4];
+            2'b10: digit = input_32bit[11:8];
+            2'b11: digit = input_32bit[15:12];
+            
             
             default: digit = 4'b0000;
         endcase
