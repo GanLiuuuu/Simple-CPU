64f160b74d67a206e062fc2d68e2a584ba88ef00
viayu <1761278446@qq.com>
2024-05-27 08:15:01 +0800

    Update instruction_fetch.v

--- instruction_fetch.v
+++ instruction_fetch.v
@@ -1,10 +1,10 @@
 `timescale 1ns / 1ps
 
 module instruction_fetch(
-   input wire clk,             // 鏃堕挓淇″彿
-   input wire rst,             // 澶嶄綅淇″彿
-   input wire [31:0] PC,       // 绋嬪簭璁℃暟鍣�
-   output [31:0] instruction // 杈撳嚭鎸囦护
+   input wire clk,               // 时钟信号
+   input wire rst,              // 复位信号
+   input wire [31:0] PC,             // 程序计数器
+   output [31:0] instruction  // 输出指令
     );
     
     instructionROM inst_rom (
